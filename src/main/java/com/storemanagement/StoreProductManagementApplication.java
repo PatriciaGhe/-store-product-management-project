5fd6c60d8621fa2cb4169222179ce2ecb3e1113b
PatriciaGhe <patricia_gheorghiescu@yahoo.com>
2024-07-22 16:05:56 +0200

    Initial commit

---
+++ src/main/java/com/storemanagement/StoreProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.storemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProductManagementApplication.class, args);
+	}
+
+}
